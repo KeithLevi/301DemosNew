568c8a2608286b5646ba638eff02ea3abe91a9fd
Keith Levi <guilddrive@gmail.com>
2021-03-13 08:53:00 -0600

    Update timer.js

--- homeworkSolutions/w3d5events/timer.js
+++ homeworkSolutions/w3d5events/timer.js
@@ -1,19 +1,32 @@
-
-
 /* need event handler for go button that reads the seconds and sets timer for that number of seconds, then calls function to change background-color */
 /* eslint-disable */
 
 
-function pageLoad() { document.getElementById("go").onclick = goClick; } 
+function pageLoad() { document.getElementById("go").onclick = goClick; }
 
-function goClick() { 
-    const seconds = +document.getElementById("sec").value;
-    setTimeout(alarm, seconds * 1000);
-} 
+function goClick() {
+    setTimeout(countDown, 1000);
+}
 
-function alarm(){
+function alarm() {
     const bodyElement = document.getElementById("body");
     bodyElement.className = "awesome";
+}
+
+/**
+ * make the textbox count down the seconds
+ * get the current value and reduce it to 0
+ * if value is 0 cancel the timer
+ */
+function countDown() {
+    secondsTextbox = document.getElementById("sec")
+    const seconds = +secondsTextbox.value;
+    if (seconds > 0) {
+        secondsTextbox.value = seconds - 1;
+        setTimeout(countDown, 1000);
+    } else {
+        alarm();
+    }
 }
 
 window.onload = pageLoad;
